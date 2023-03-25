27000f6b5d18a5b553132d6bcd35802e310dc941
sajitharedd16 <68114878+sajitharedd16@users.noreply.github.com>
2023-03-25 18:16:49 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,35 +1,44 @@
 const todoList = () => {
   all = [];
   const add = (todoItem) => {
-    all.push(todoItem)
+    all.push(todoItem);
   };
   const markAsComplete = (index) => {
-    all[index].completed = true
+    all[index].completed = true;
   };
 
   const overdue = () => {
+    // Write the date check condition here and return the array
+    // of overdue items accordingly.
     return all.filter((item) => {
       return item.dueDate < today;
     });
   };
 
   const dueToday = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due today accordingly.
+
     return all.filter((item) => {
-      return item.dueDate === today;
-   });
+       return item.dueDate === today;
+    });
   };
 
   const dueLater = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due later accordingly.
     return all.filter((item) => {
-      return item.dueDate > today;
-   });
+       return item.dueDate > today;
+    });
   };
 
   const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
     return list.map((item) => {
-      return ${item.completed ? "[x]" : "[ ]"} ${item.title} ${
+      return `${item.completed ? "[x]" : "[ ]"} ${item.title} ${
         item.dueDate === today ? "" : item.dueDate
-      };
+      }`;
     }).join("\n");
   };
 
@@ -40,10 +49,9 @@ const todoList = () => {
     overdue,
     dueToday,
     dueLater,
-    toDisplayableList
+    toDisplayableList,
   };
 };
-
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
 // ####################################### #
