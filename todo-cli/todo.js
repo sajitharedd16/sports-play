f7bc240e1119f4153b62c680d73e7f8a5d6d56b0
sajitharedd16 <68114878+sajitharedd16@users.noreply.github.com>
2023-04-08 11:00:02 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-var */
-/* eslint-disable eqeqeq */
-/* eslint-disable no-undef */
 const todoList = () => {
   all = []
   const add = (todoItem) => {
@@ -49,39 +46,4 @@ const todoList = () => {
   }
 }
 
-const todos = todoList()
-
-const formattedDate = (d) => {
-  return d.toISOString().split('T')[0]
-}
-
-var d = new Date()
-const today = formattedDate(d)
-const yesterday = formattedDate(new Date(d.setDate(d.getDate() - 1)))
-const tomorrow = formattedDate(new Date(d.setDate(d.getDate() + 2)))
-
-todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-
-console.log('My Todo-list\n')
-
-console.log('Overdue')
-var overdues = todos.overdue()
-var formattedOverdues = todos.toDisplayableList(overdues)
-console.log(formattedOverdues)
-console.log()
-
-console.log('Due Today')
-let itemsDueToday = todos.dueToday()
-let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-console.log(formattedItemsDueToday)
-console.log()
-
-console.log('Due Later')
-let itemsDueLater = todos.dueLater()
-let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-console.log(formattedItemsDueLater)
-
 module.exports = todoList
- 
