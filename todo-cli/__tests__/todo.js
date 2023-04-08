0625147a37194272f5a5a66c87ee6b6855d5b05e
sajitharedd16 <68114878+sajitharedd16@users.noreply.github.com>
2023-04-08 13:18:55 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,58 +1,60 @@
-let todoList = require("../todo");
-const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
-describe("Todo test cases", () => {
+/* eslint-disable no-undef */
+const todoList = require('../todo')
+
+const { all, markAsComplete, add, overdue, dueLater, dueToday } = todoList()
+
+const formattedDate = (d) => {
+  return d.toISOString().split('T')[0]
+};
+const Todaydate = new Date()
+
+describe('ToDolist Test Suite', () => {
   beforeAll(() => {
-    const today = new Date();
-    const oneDay = 60 * 60 * 24 * 1000;
-    [
-      {
-        title: "first test",
-        completed: false,
-        dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
-          "en-CA"
-        ),
-      },
-      {
-        title: "second test",
-        completed: false,
-        dueDate: new Date().toLocaleDateString("en-CA"),
-      },
-      {
-        title: "third test",
-        completed: false,
-        dueDate: new Date(today.getTime() + 1 * oneDay).toLocaleDateString(
-          "en-CA"
-        ),
-      },
-    ].forEach(add);
-  });
-  test("creating a new todo", () => {
-    expect(all.length).toEqual(3);
-
     add({
-      title: "Take the test",
+      title: 'new todo',
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-
-    expect(all.length).toEqual(4);
-  });
-
-  test("marking a todo as completed", () => {
-    expect(all[0].completed).toEqual(false);
-    markAsComplete(0);
-    expect(all[0].completed).toEqual(true);
-  });
-
-  test("retrieval of overdue items test", () => {
-    expect(overdue().length).toEqual(1);
-  });
-
-  test("retrieval of due today items test", () => {
-    expect(dueToday().length).toEqual(2);
-  });
-
-  test("retrieval of due later items test", () => {
-    expect(dueLater().length).toEqual(1);
-  });
-});
+      dueDate: new Date().toISOString().slice(0, 10)
+    })
+  })
+  test('creating a new todo', () => {
+    const countToDoitems = all.length
+    add({
+      title: 'Test todo',
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10)
+    })
+    expect(all.length).toBe(countToDoitems + 1)
+  })
+  test('marking a todo as completed', () => {
+    expect(all[0].completed).toBe(false)
+    markAsComplete(0)
+    expect(all[0].completed).toBe(true)
+  })
+  test('retrieval of overdue items test', () => {
+    const sampledate = formattedDate(
+      new Date(new Date().setDate(Todaydate.getDate() - 1))
+    )
+    const sampletodo = {
+      title: 'Sample overdue testcase',
+      dueDate: sampledate,
+      completed: false
+    }
+    add(sampletodo)
+    expect(overdue().length).toBe(1)
+  })
+  test('retrieval of due later items test', () => {
+    const sampledate = formattedDate(
+      new Date(new Date().setDate(Todaydate.getDate() + 1))
+    )
+    const sampletodo = {
+      title: 'Sample due later testcase',
+      dueDate: sampledate,
+      completed: false
+    }
+    add(sampletodo)
+    expect(dueLater().length).toBe(1)
+  })
+  test('retrieval of due today items test', () => {
+    expect(dueToday().length).toEqual(2)
+  })
+})
