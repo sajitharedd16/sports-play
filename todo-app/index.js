a7a6316d31ca886fcf160b6ec5339b2ff302fccc
sajithareddy16 <68114878+sajitharedd16@users.noreply.github.com>
2023-05-06 19:34:10 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,4 +1,5 @@
-const app = require('./app');
-app.listen(3000,() =>{
-    console.log("Started express server at port 3000")
-})
+const app = require("./app");
+
+app.listen(3300, () => {
+  console.log("Started express server at port 3300");
+});
