57c7a1258255641b4617bff4807dcdf683629790
sajithareddy16 <68114878+sajitharedd16@users.noreply.github.com>
2023-05-28 13:08:06 +0530

    Update create_session.js

--- models/create_session.js
+++ models/create_session.js
@@ -2,11 +2,6 @@
 const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Create_session extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
